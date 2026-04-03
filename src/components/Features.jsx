f70a9f3def531e4b895bd4900780d8e5577351dd
rupak <rupak3609n@gmail.com>
2026-04-03 12:53:27 +0545

    Added cards to Features component

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,24 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Features = () => {
+
+    const [number, setnumber] = useState(1)
   return (
-    <section>
+    <section className='px-20 py-16'>
         <div>
-            <h2 className='text-4xl font-bold text-center'>Features</h2>
+            <h2 className='text-4xl font-bold text-center mb-8'>Features {number}</h2>
+
+            <button onClick={()=>{setnumber(number + 1)}}>add</button>
         </div>
 
-        <div className='grid grid-cols-3'>
-            <div>
-                <h3>Real Time Response</h3>
-                <p>Get instant feedback on your actions and decisions.</p>
+        <div className='grid grid-cols-3 gap-6'>
+            <div className='border border-gray-100 rounded-lg p-6 shadow-xl shadow-blue-300'>
+                <h3 className='text-xl font-semibold mb-4'>Real Time Response</h3>
+                <p className='text-lg font-medium text-gray-600'>Get instant feedback on your actions and decisions.</p>
             </div>
-            <div>
-                <h3>Advanced Analytics</h3>
-                <p>Gain insights from your  data with our powerful analytics tools.</p>
+            <div className='border border-gray-100 rounded-lg p-6 shadow-xl  shadow-blue-300'>
+                <h3 className='text-xl font-semibold mb-4'>Advanced Analytics</h3>
+                <p className='text-lg font-medium text-gray-600'>Gain insights from your  data with our powerful analytics tools.</p>
             </div>            
-            <div>
-                <h3>Personalized Experience</h3>
-                <p>Enjoy a tailored experience that adapts to your needs and preferences.</p>
+            <div className='border border-gray-100 rounded-lg p-6 shadow-xl  shadow-blue-300'>
+                <h3 className='text-xl font-semibold mb-4'>Personalized Experience</h3>
+                <p className='text-lg font-medium text-gray-600'>Enjoy a tailored experience that adapts to your needs and preferences.</p>
             </div>
         </div>
     </section>
